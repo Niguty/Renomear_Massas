b1e215ed384507239eaee21bbabeed669b57ee1b
LucasDNiguti <niguti005@gmail.com>
2025-02-20 13:00:44 -0300

    Update os.py

--- os.py
+++ os.py
@@ -3,5 +3,12 @@ import os
 os.chdir('C:\\Teste')
 print(f'Diretório atual: {os.getcwd()}')
 
-padra_nome = input('Qual o padrão de nomes de arquivos a usar (sem extensão)? ')
+padrao_nome = input('Qual o padrão de nomes de arquivos a usar (sem extensão)? ')
 
+for contador, arq in enumerate(os.listdir()):
+    if os.path.isfile(arq):
+        nome_arq, exten_arq = os.path.splitext(arq)
+        nome_arq = padrao_nome + ' ' + str(contador)
+
+        nome_novo = f'{nome_arq}{exten_arq}'
+        os.rename(arq, nome_novo)
